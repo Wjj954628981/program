52f9d15409eb52af6f1c84a6eb31b488f748f701
Wordsworth <wjj954628981@163.com>
2017-03-20 20:03:30 +0800

    renren zhaopin the written test online uses java workplace. Always have to commit java workplace change, it's a bad repository, but i can't change it. sad..

---
+++ java/workplace/java/src/test3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class test3 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int n = cin.nextInt();
+		String[] list = new String[n];
+		String[] output = new String[n];
+		for(int i=0;i<n;i++){
+			list[i] = cin.nextLine();
+		}
+		for(int i=0;i<n;i++){
+			if(list[i].contains("+")||list[i].contains("-")){
+				for(int j=0;j<list[i].length();j++){
+					if(String.valueOf(list[i].charAt(j)).equals("+")){
+						output[i]+="/\\";
+					}else{
+						output[i]+="__";
+					}
+				}
+			}else{
+				for(int k=0;k<list[i].length();k++){
+					if(String.valueOf(list[i].charAt(k)).equals("_")){
+						output[i]+="-";
+						k++;
+					}else{
+						output[i]+="+";
+						k++;
+					}
+				}
+			}
+		}
+		for(int i=0;i<n;i++){
+			System.out.println(output[i]);
+		}
+	}
+}
---
+++ java/workplace/java/src/test5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test5 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int b = cin.nextInt();
+		while(true){
+			int i=1;
+			
+		}
+	}
+}
