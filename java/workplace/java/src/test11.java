027a9feb5b03ab83265504c07554b410a05a6da8
Wordsworth <wjj954628981@163.com>
2017-03-30 21:06:22 +0800

    java zhaopin...

---
+++ java/workplace/java/src/test11.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class test11 {
+
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		String[] a = cin.nextLine().split("");
+		int rank = 0;
+		List<String> charList = new ArrayList<String>();
+		for(int i=0;i<a.length;i++){
+			if(charList.size()==0){
+				
+			}
+			if(a[i].equals("[")){
+				charList.add("[");
+			}else{
+				rank++;
+				charList.remove(charList.size()-1);
+			}
+		}
+	}
+
+}
